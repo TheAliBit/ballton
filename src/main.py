22565c6ad6bca2a7ff5870f5a888785ca7297225
Ali8Bits <the.ali8bits@gmail.com>
2024-09-19 09:47:20 -0700

    feat: can move the ball with wasd and cannot go out of the display

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ clock = pygame.time.Clock()
 
 ball = pygame.image.load('ball.png').convert_alpha()
 ball = pygame.transform.scale(ball, (80, 80))
+ball_rect = ball.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
 
 running = True
 
@@ -20,7 +21,28 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_ESCAPE]:
+        running = False
+    speed = 10
+    if keys[pygame.K_w]:
+        if ball_rect.top > 0:
+            ball_rect.y -= speed
+
+    if keys[pygame.K_s]:
+        if ball_rect.bottom < SCREEN_HEIGHT:
+            ball_rect.y += speed
+
+    if keys[pygame.K_a]:
+        if ball_rect.left > 0:
+            ball_rect.x -= speed
+
+    if keys[pygame.K_d]:
+        if ball_rect.right < SCREEN_WIDTH:
+            ball_rect.x += speed
+
+
     screen.fill((0, 0, 0))
-    screen.blit(ball, (0, 0))
+    screen.blit(ball, (ball_rect))
     pygame.display.update()
     clock.tick(60)
